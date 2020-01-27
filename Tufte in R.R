fa913e0f69395a461e272d08c63a5e8592377689
Tanay Mukherjee <its_tanay_here@yahoo.com>
2020-01-27 04:12:31 -0500

    Update Tufte in R.R

--- Tufte in R.R
+++ Tufte in R.R
@@ -2,7 +2,7 @@
 # 26th Jan, 2020
 
 # install.packages(c("CarletonStats", "devtools", "epanetReader", "fmsb", "ggplot2", "ggthemes", 
-#                    "latticeExtra", "MASS", "PerformanceAnalytics", "psych", "highcharter", "digest",
+#                    "latticeExtra", "MASS", "PerformanceAnalytics", "psych", 
 #                    "plyr", "prettyR", "plotrix", "proto", "RCurl", "reshape", "reshape2"))
 
 
@@ -198,3 +198,260 @@ p <- ggplot(faithful, aes(waiting, eruptions)) + geom_point() + theme_tufte(tick
   theme(axis.title=element_blank(), axis.text=element_blank())
 ggMarginal(p, type = "boxplot", size=10, fill="transparent")
 
+
+# Minimal boxplot
+
+# Minimal boxplot in base graphics
+x <- quakes$mag
+y <- quakes$stations
+boxplot(y ~ x, main = "", axes = FALSE, xlab=" ", ylab=" ",
+        pars = list(boxcol = "transparent", medlty = "blank", medpch=16, whisklty = c(1, 1),
+                    medcex = 0.7,  outcex = 0, staplelty = "blank"))
+axis(1, at=1:length(unique(x)), label=sort(unique(x)), tick=F, family="serif")
+axis(2, las=2, tick=F, family="serif")
+text(min(x)/3, max(y)/1.1, pos = 4, family="serif",
+     "Number of stations \nreporting Richter Magnitude\nof Fiji earthquakes (n=1000)")
+
+
+# Minimal boxplot in base graphics with chart.Boxplot
+
+library(PerformanceAnalytics)
+library(psych)
+d <- msq[,80:84]
+chart.Boxplot(d, main = "", xlab="average personality rating (based on n=3896)", ylab="", 
+              element.color = "transparent", as.Tufte=TRUE)
+
+
+#Minimal boxplot in lattice
+x <- quakes$mag
+y <- quakes$stations
+bwplot(y ~ x, horizontal=F, xlab="", ylab="", do.out = FALSE, box.ratio = 0,
+       scales=list(x=list(labels=sort(unique(x)), fontfamily="serif"),
+                   y=list(fontfamily="serif")),
+       par.settings = list(axis.line = list(col = "transparent"), box.umbrella=list(lty=1, col= 1),
+                           box.dot=list(col= 1), box.rectangle = list(col= c("transparent"))))
+ltext(current.panel.limits()$xlim[1]+250, adj=1,
+      current.panel.limits()$ylim[2]+50, fontfamily="serif",
+      "Number of stations \nreporting Richter Magnitude\nof Fiji earthquakes (n=1000)")
+
+
+# Minimal boxplot in ggplot2
+library(ggplot2)
+library(ggthemes)
+ggplot(quakes, aes(factor(mag),stations)) + theme_tufte() +
+  geom_tufteboxplot(outlier.colour="transparent") + theme(axis.title=element_blank()) +
+  annotate("text", x = 8, y = 120, adj=1,  family="serif",
+           label = c("Number of stations \nreporting Richter Magnitude\nof Fiji earthquakes (n=1000)"))
+
+
+# Minimal barchart
+
+# library(psych)
+d <- colMeans(msq[,c(2,7,34,36,42,43,46,55,68)], na.rm = T)*10
+barplot(d, xaxt="n", yaxt="n", ylab="", border=F, width=c(.35), space=1.8)
+axis(1, at=(1:length(d))-.26, labels=names(d), tick=F, family="serif")
+axis(2, at=seq(1, 5, 1), las=2, tick=F, family="serif")
+abline(h=seq(1, 5, 1), col="white", lwd=3)
+abline(h=0, col="gray", lwd=2)
+text(min(d)/2, max(d)/1.2, pos = 4, family="serif",
+     "Average scores\non negative emotion traits\nfrom 3896 participants\n(Watson et al., 1988)")
+
+# Minimal barchart in lattice
+
+library(lattice)
+library(psych)
+d <- colMeans(msq[,c(2,7,34,36,42,43,46,55,68)],na.rm = T)*10
+barchart(sort(d), xlab="", ylab="", col = "grey", origin=1,  
+         border = "transparent", box.ratio=0.5, 
+         panel = function(x,y,...) {
+           panel.barchart(x,y,...)
+           panel.abline(v=seq(1,6,1), col="white", lwd=3)},
+         par.settings = list(axis.line = list(col = "transparent")))
+ltext(current.panel.limits()$xlim[2]-50, adj=1,  
+      current.panel.limits()$ylim[1]-100,
+      "Average scores\non negative emotion traits\nfrom 3896 participants\n(Watson et al., 1988)")
+
+# Minimal barchart in ggplot2
+
+library(ggplot2)
+library(ggthemes)
+library(psych)
+library(reshape2)
+d <- melt(colMeans(msq[,c(2,7,34,36,42,43,46,55,68)],na.rm = T)*10)
+d$trait <- rownames(d)
+ggplot(d, aes(x=trait, y=value)) + theme_tufte(base_size=14, ticks=F) +
+  geom_bar(width=0.25, fill="gray", stat = "identity") +  theme(axis.title=element_blank()) +
+  scale_y_continuous(breaks=seq(1, 5, 1)) + 
+  geom_hline(yintercept=seq(1, 5, 1), col="white", lwd=1) +
+  annotate("text", x = 3.5, y = 5, adj=1,  family="serif",
+           label = c("Average scores\non negative emotion traits
+          from 3896 participants\n(Watson et al., 1988)"))
+
+# Minimal barchart - interactive with highcharter
+library(psych)
+library(reshape)
+library(highcharter)
+values <- 1 + abs(rnorm(12))
+d <- melt(colMeans(msq[,c(2,7,34,36,42,43,46,55,68)], na.rm = T)*10)
+trait <- row.names(d) 
+value <- as.vector(d[,1])
+highchart() %>%
+  hc_chart(type = "column") %>%
+  hc_add_series(data = value) %>%
+  hc_xAxis(categories = row.names(d)) %>%
+  hc_add_theme(hc_theme_tufte2())
+
+
+
+# Slopegraph
+
+# Slopegraph in base graphics
+
+library(devtools)
+#install_github("leeper/slopegraph")#install Leeper's package from Github
+library(slopegraph)
+data(cancer)
+slopegraph(cancer, col.lines = 'gray', col.lab = 1, col.num = 1,
+           xlim = c(-.2,5),
+           main = "Estimate of % survival rates",
+           xlabels = c('5 Year','10 Year','15 Year','20 Year'))
+
+# Slopegraph in ggplot2 with plot_slopegraph
+# library(ggplot2)
+# library(ggthemes)
+# library(devtools)
+# library(RCurl)
+# library(plyr)
+# source_url("https://raw.githubusercontent.com/jkeirstead/r-slopegraph/master/slopegraph.r")
+# d <- read.csv(text = getURL("https://raw.githubusercontent.com/jkeirstead/r-slopegraph/master/cancer_survival_rates.csv"))
+# df <- build_slopegraph(d, x="year", y="value", group="group", method="tufte", min.space=0.04)
+# df <- transform(df, x=factor(x, levels=c(5,10,15,20),
+#                              labels=c("5 years","10 years","15 years","20 years")), y=round(y))
+# plot_slopegraph(df) + labs(title="Estimates of % survival rates") +
+#   theme_tufte(base_size=16, ticks=F) + theme(axis.title=element_blank())
+
+
+# Sparklines
+
+# Sparklines in base graphics
+
+library(RCurl)
+dd <- read.csv(text = getURL("https://gist.githubusercontent.com/GeekOnAcid/da022affd36310c96cd4/raw/9c2ac2b033979fcf14a8d9b2e3e390a4bcc6f0e3/us_nr_of_crimes_1960_2014.csv"))
+d <- dd[,c(2:11)]
+pdf("sparklines_base.pdf", height=10, width=6)
+par(mfrow=c(ncol(d),1), mar=c(1,0,0,8), oma=c(4,1,4,4))
+for (i in 1:ncol(d)){
+  plot(d[,i], lwd=0.5, axes=F, ylab="", xlab="", main="", type="l", new=F)
+  axis(4, at=d[nrow(d),i], labels=round(d[nrow(d),i]), tick=F, las=1, line=-1.5, 
+       family="serif", cex.axis=1.2)
+  axis(4, at=d[nrow(d),i], labels=names(d[i]), tick=F, line=1.5, 
+       family="serif", cex.axis=1.4, las=1)
+  text(which.max(d[,i]), max(d[,i]), labels=round(max(d[,i]),0), 
+       family="serif", cex=1.2, adj=c(0.5,3))
+  text(which.min(d[,i]), min(d[,i]), labels=round(min(d[,i]),0), 
+       family="serif", cex=1.2, adj=c(0.5,-2.5))
+  ymin <- min(d[,i]); tmin <- which.min(d[,i]); ymax<-max(d[,i]); tmax<-which.max(d[,i]);
+  points(x=c(tmin,tmax), y=c(ymin,ymax), pch=19, col=c("red","blue"), cex=1)
+  rect(0, summary(d[,i])[2], nrow(d), summary(d[,i])[4], border=0, 
+       col = rgb(190, 190, 190, alpha=90, maxColorValue=255))}
+axis(1, at=1:nrow(dd), labels=dd$Year, pos=c(-5), tick=F, family="serif", cex.axis=1.4)
+dev.off()
+
+
+# Sparklines in base graphics with plotSparklineTable
+library(epanetReader)
+library(reshape)
+library(RCurl)
+dd <- read.csv(text = getURL("https://gist.githubusercontent.com/GeekOnAcid/da022affd36310c96cd4/raw/9c2ac2b033979fcf14a8d9b2e3e390a4bcc6f0e3/us_nr_of_crimes_1960_2014.csv"))
+d <- melt(dd[,c(2:11)])
+pdf("sparklines_base_epanetReader.pdf", height=6, width=10)
+plotSparklineTable(d, row.var = 'variable', col.vars = 'value')
+
+
+# Sparklines in lattice
+library(lattice)
+library(latticeExtra)
+library(grid)
+library(reshape)
+library(RCurl)
+dd <- read.csv(text = getURL("https://gist.githubusercontent.com/GeekOnAcid/da022affd36310c96cd4/raw/9c2ac2b033979fcf14a8d9b2e3e390a4bcc6f0e3/us_nr_of_crimes_1960_2014.csv"))
+d <- melt(dd, id="Year")
+names(d)[1] <- "time"
+pdf("sparklines_lattice.pdf", height=10, width=8)
+xyplot(value~time | variable, d, xlab="", ylab="", strip=F, lwd=0.7, col=1, type="l",
+       layout=c(1,length(unique(d$variable))), between = list(y = 1),
+       scales=list(y=list(at=NULL, relation="free"), x=list(fontfamily="serif")),
+       par.settings = list(axis.line = list(col = "transparent"),
+                           layout.widths=list(right.padding=20, left.padding=-5)),
+       panel = function(x, y, ...) {
+         panel.xyplot(x, y, ...)
+         pushViewport(viewport(xscale=current.viewport()$xscale-5,
+                               yscale=current.viewport()$yscale, clip="off"))
+         panel.text(x=tail(x,n=1), y=tail(y,n=1), labels=levels(d$variable)[panel.number()],
+                    fontfamily="serif", pos=4)
+         popViewport()
+         panel.text(x=x[which.max(y)], y=max(y), labels=round(max(y),0), cex=0.8,
+                    fontfamily="serif",adj=c(0.5,2.5))
+         panel.text(x=x[which.min(y)], y=min(y), labels=round(min(y),0), cex=0.8,
+                    fontfamily="serif",adj=c(0.5,-1.5))
+         panel.text(x=tail(x,n=1), y=tail(y,n=1), labels=round(tail(y,n=1),0), cex=0.8,
+                    fontfamily="serif", pos=4)
+         panel.points(x[which.max(y)], max(y),  pch=16, cex=1)
+         panel.points(x[which.min(y)], min(y),  pch=16, cex=1, col="red")
+         panel.rect(min(x), quantile(y, 0.25), max(x), quantile(y, 0.75),
+                    col = "grey", border = "transparent", alpha = 0.4)
+       })
+dev.off()
+
+# Sparklines in ggplot2
+library(ggplot2)
+library(ggthemes)
+library(dplyr)
+library(reshape)
+library(RCurl)
+dd <- read.csv(text = getURL("https://gist.githubusercontent.com/GeekOnAcid/da022affd36310c96cd4/raw/9c2ac2b033979fcf14a8d9b2e3e390a4bcc6f0e3/us_nr_of_crimes_1960_2014.csv"))
+d <- melt(dd, id="Year")
+names(d) <- c("Year","Crime.Type","Crime.Rate")
+d$Crime.Rate <- round(d$Crime.Rate,0)
+mins <- group_by(d, Crime.Type) %>% slice(which.min(Crime.Rate))
+maxs <- group_by(d, Crime.Type) %>% slice(which.max(Crime.Rate))
+ends <- group_by(d, Crime.Type) %>% filter(Year == max(Year))
+quarts <- d %>% group_by(Crime.Type) %>%
+  summarize(quart1 = quantile(Crime.Rate, 0.25),
+            quart2 = quantile(Crime.Rate, 0.75)) %>%
+  right_join(d)
+pdf("sparklines_ggplot.pdf", height=10, width=8)
+ggplot(d, aes(x=Year, y=Crime.Rate)) + 
+  facet_grid(Crime.Type ~ ., scales = "free_y") + 
+  geom_ribbon(data = quarts, aes(ymin = quart1, max = quart2), fill = 'grey90') +
+  geom_line(size=0.3) +
+  geom_point(data = mins, col = 'red') +
+  geom_point(data = maxs, col = 'blue') +
+  geom_text(data = mins, aes(label = Crime.Rate), vjust = -1) +
+  geom_text(data = maxs, aes(label = Crime.Rate), vjust = 2.5) +
+  geom_text(data = ends, aes(label = Crime.Rate), hjust = 0, nudge_x = 1) +
+  geom_text(data = ends, aes(label = Crime.Type), hjust = 0, nudge_x = 5) +
+  expand_limits(x = max(d$Year) + (0.25 * (max(d$Year) - min(d$Year)))) +
+  scale_x_continuous(breaks = seq(1960, 2010, 10)) +
+  scale_y_continuous(expand = c(0.1, 0)) +
+  theme_tufte(base_size = 15, base_family = "Helvetica") +
+  theme(axis.title=element_blank(), axis.text.y = element_blank(), 
+        axis.ticks = element_blank(), strip.text = element_blank())
+dev.off()
+
+
+# Stem-and-leaf display
+
+#Stem-and-leaf display in console with base graphics
+stem(faithful$eruptions)
+
+# Stem-and-leaf display in console with CarletonStats
+library(CarletonStats)
+library(MASS)
+stemPlot(birthwt$bwt, birthwt$smoke, varname="infant birth weight (in grams)",
+         grpvarname="whether mother smoked during pregnancy (1) or not (0)")
+
+
+# Stem-and-leaf display in base graphics with fmsb
+library(fmsb)
+gstem(faithful$eruptions)
